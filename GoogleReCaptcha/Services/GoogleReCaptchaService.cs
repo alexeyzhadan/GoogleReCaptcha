5eb7532a8a4b864b3aef4dea9827d2177b35ba62
Alexey Zhadan <alexey.zhadan@ukad-group.com>
2021-01-18 08:08:28 -0800

    change GetResultVerification

--- GoogleReCaptcha/Services/GoogleReCaptchaService.cs
+++ GoogleReCaptcha/Services/GoogleReCaptchaService.cs
@@ -1,6 +1,7 @@
 ﻿using GoogleReCaptcha.Models;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -8,7 +9,7 @@ namespace GoogleReCaptcha.Services
 {
     public class GoogleReCaptchaService
     {
-        private const string STRING_FORMAT_OF_PATH_VERIFIED_API = "https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}";
+        private const string PATH_VERIFIED_API = "https://www.google.com/recaptcha/api/siteverify";
 
         private readonly ReCaptchaSettings _settings;
 
@@ -20,16 +21,39 @@ namespace GoogleReCaptcha.Services
         public async Task<GoogleResponse> GetResultOfVerification(string token)
         {
             GoogleResponse googleResponse;
+            var data = new KeyValuePair<string, string>[]
+            {
+                new KeyValuePair<string, string>("secret", _settings.SecretKey),
+                new KeyValuePair<string, string>("response", token)
+            };
+
 
             using (var client = new HttpClient())
             {
-                var response = await client.GetStringAsync(
-                    string.Format(STRING_FORMAT_OF_PATH_VERIFIED_API, _settings.SecretKey, token));
+                var response = await client.PostAsync(PATH_VERIFIED_API, new FormUrlEncodedContent(data));
+                var responseAsString = await response.Content.ReadAsStringAsync();
 
-                googleResponse = JsonConvert.DeserializeObject<GoogleResponse>(response);
+                googleResponse = JsonConvert.DeserializeObject<GoogleResponse>(responseAsString);
             }
 
             return googleResponse;
         }
+
+        // 2
+        //private const string STRING_FORMAT_OF_PATH_VERIFIED_API = "https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}";
+        //public async Task<GoogleResponse> GetResultOfVerification(string token)
+        //{
+        //    GoogleResponse googleResponse;
+
+        //    using (var client = new HttpClient())
+        //    {
+        //        var response = await client.GetStringAsync(
+        //            string.Format(STRING_FORMAT_OF_PATH_VERIFIED_API, _settings.SecretKey, token));
+
+        //        googleResponse = JsonConvert.DeserializeObject<GoogleResponse>(response);
+        //    }
+
+        //    return googleResponse;
+        //}
     }
 }
